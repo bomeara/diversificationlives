1696fc397a603edbc4b9a3103b2eeb0aec02312b
Brian O'Meara <omeara.brian@gmail.com>
2020-05-17 00:21:36 -0400

    discrete shift models

--- WhaleDrake/R/functions.R
+++ WhaleDrake/R/functions.R
@@ -110,3 +110,100 @@ param_lambda7p_mu_multiplier_lambda <- function(desired_interval = 0.1, tree, co
     })
     return(list(fit_param=fit_param, lambda_function=lambda_function, mu_function=mu_function, age_grid_param=age_grid_param))
 }
+
+# desired_interval: how finely to divide the slices for calculating smoothly changing (or not) lambda and mu
+# slice_ages: when to divide regimes
+# interpolation_method: same as method in ?approx. Constant or linear
+param_lambda_discreteshift_mu_discreteshift <- function(desired_interval = 0.1, tree, condition="crown", ncores=parallel::detectCores(), slice_ages = seq(from=0, to=floor(castor::get_tree_span(tree)$max_distance), by=1), interpolation_method="constant") {
+    root_age = castor::get_tree_span(tree)$max_distance
+    rho = 1
+    age_grid_param = seq(from=0,to=root_age+desired_interval,by=desired_interval)
+
+    lambda_params <- rep(NA, length(slice_ages))
+    names(lambda_params) <- paste0("lambda", sequence(length(slice_ages))-1)
+
+    mu_params <- rep(NA, length(slice_ages))
+    names(mu_params) <- paste0("mu", sequence(length(slice_ages))-1)
+
+    lambda_function = function(ages,params){
+        results <- stats::approx(x=slice_ages, y=params[grepl("lambda", names(params))], xout=ages, method=interpolation_method)
+       return(results)
+    }
+
+    mu_function = function(ages,params){
+        results <- stats::approx(x=slice_ages, y=params[grepl("mu", names(params))], xout=ages, method=interpolation_method)
+       return(results)
+    }
+
+
+    rho_function = function(params){
+        return(rho) # rho does not depend on any of the parameters
+    }
+    param_values <- c(lambda_params, mu_params)
+    param_guess <- c(rep(0.1, length(lambda_params)), rep(0.01, length(mu_params)))
+    fit_param = NA
+    try({
+        fit_param <- fit_hbd_model_parametric(	tree,
+                                               param_values  = param_values,
+                                               param_guess   = param_guess,
+                                               param_min     = rep(0, length(param_values)),
+                                               param_max     = rep(2, length(param_values)),
+                                               param_scale   = 1, # all params are in the order of 1
+                                               lambda        = lambda_function,
+                                               mu            = mu_function,
+                                               rho0          = rho_function,
+                                               age_grid      = age_grid_param,
+                                               condition     = condition,
+                                               Ntrials       = 10,    # perform 10 fitting trials
+                                               Nthreads      = ncores,
+                                               fit_control       = list(rel.tol=1e-6)
+                                           )
+    })
+    return(list(fit_param=fit_param, lambda_function=lambda_function, mu_function=mu_function, age_grid_param=age_grid_param))
+}
+
+param_lambda_discreteshift_ef_fixed <- function(desired_interval = 0.1, tree, condition="crown", ncores=parallel::detectCores(), slice_ages = seq(from=0, to=floor(castor::get_tree_span(tree)$max_distance), by=1), interpolation_method="constant", ef=0.0) {
+    root_age = castor::get_tree_span(tree)$max_distance
+    rho = 1
+    age_grid_param = seq(from=0,to=root_age+desired_interval,by=desired_interval)
+
+    lambda_params <- rep(NA, length(slice_ages))
+    names(lambda_params) <- paste0("lambda", sequence(length(slice_ages))-1)
+
+
+    lambda_function = function(ages,params){
+        results <- stats::approx(x=slice_ages, y=params[grepl("lambda", names(params))], xout=ages, method=interpolation_method)
+       return(results)
+    }
+
+    mu_function = function(ages,params){
+        results <- ef*lambda_function(ages, params)
+        return(results)
+    }
+
+
+    rho_function = function(params){
+        return(rho) # rho does not depend on any of the parameters
+    }
+    param_values <- c(lambda_params)
+    param_guess <- c(rep(0.1, length(lambda_params)))
+    fit_param = NA
+    try({
+        fit_param <- fit_hbd_model_parametric(	tree,
+                                               param_values  = param_values,
+                                               param_guess   = param_guess,
+                                               param_min     = rep(0, length(param_values)),
+                                               param_max     = rep(2, length(param_values)),
+                                               param_scale   = 1, # all params are in the order of 1
+                                               lambda        = lambda_function,
+                                               mu            = mu_function,
+                                               rho0          = rho_function,
+                                               age_grid      = age_grid_param,
+                                               condition     = condition,
+                                               Ntrials       = 10,    # perform 10 fitting trials
+                                               Nthreads      = ncores,
+                                               fit_control       = list(rel.tol=1e-6)
+                                           )
+    })
+    return(list(fit_param=fit_param, lambda_function=lambda_function, mu_function=mu_function, age_grid_param=age_grid_param))
+}
--- WhaleDrake/R/plan.R
+++ WhaleDrake/R/plan.R
@@ -11,11 +11,24 @@ plan <- drake_plan(
 
     fit_param_lambda7p_mu_multiplier_lambda_stem = param_lambda7p_mu_multiplier_lambda(desired_interval = 0.1, tree=tree, condition="stem"),
 
+    fit_param_lambda_discreteshift_mu_discreteshift_crown_1my_constant = param_lambda_discreteshift_mu_discreteshift(desired_interval = 0.1, tree=tree, condition="crown", interpolation_method="constant", slice_ages = seq(from=0, to=floor(castor::get_tree_span(tree)$max_distance), by=1)),
+
+    fit_param_lambda_discreteshift_mu_discreteshift_crown_1my_linear = param_lambda_discreteshift_mu_discreteshift(desired_interval = 0.1, tree=tree, condition="crown", interpolation_method="linear", slice_ages = seq(from=0, to=floor(castor::get_tree_span(tree)$max_distance), by=1)),
+
+    fit_param_lambda_discreteshift_ef_0_crown_1my_linear = param_lambda_discreteshift_ef_fixed(desired_interval = 0.1, tree=tree, condition="crown", interpolation_method="linear", slice_ages = seq(from=0, to=floor(castor::get_tree_span(tree)$max_distance), by=1), ef=0.0),
+
+    fit_param_lambda_discreteshift_ef_0_9_crown_1my_linear = param_lambda_discreteshift_ef_fixed(desired_interval = 0.1, tree=tree, condition="crown", interpolation_method="linear", slice_ages = seq(from=0, to=floor(castor::get_tree_span(tree)$max_distance), by=1), ef=0.9),
+
+
     all_results = list(
         fit_param_lambda7p_mu7p_crown=fit_param_lambda7p_mu7p_crown,
         fit_param_lambda7p_mu7p_stem=fit_param_lambda7p_mu7p_stem,
         fit_param_lambda7p_mu_multiplier_lambda_crown=fit_param_lambda7p_mu_multiplier_lambda_crown,
-        fit_param_lambda7p_mu_multiplier_lambda_stem=fit_param_lambda7p_mu_multiplier_lambda_stem
+        fit_param_lambda7p_mu_multiplier_lambda_stem=fit_param_lambda7p_mu_multiplier_lambda_stem,
+        fit_param_lambda_discreteshift_mu_discreteshift_crown_1my_constant=fit_param_lambda_discreteshift_mu_discreteshift_crown_1my_constant,
+        fit_param_lambda_discreteshift_mu_discreteshift_crown_1my_linear=fit_param_lambda_discreteshift_mu_discreteshift_crown_1my_linear,
+        fit_param_lambda_discreteshift_ef_0_crown_1my_linear=fit_param_lambda_discreteshift_ef_0_crown_1my_linear,
+        fit_param_lambda_discreteshift_ef_0_9_crown_1my_linear=fit_param_lambda_discreteshift_ef_0_9_crown_1my_linear
     ),
 
     all_results_saved = save(all_results, file=file_out("results.rda"))
