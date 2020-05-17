df2ab6fca91f1ca05d3cdde7a68d9918ca34784c
Brian O'Meara <omeara.brian@gmail.com>
2020-05-17 00:07:38 -0400

    file_out not file.out

--- WhaleDrake/R/plan.R
+++ WhaleDrake/R/plan.R
@@ -1,5 +1,6 @@
 # Note that drake will not work with multiple cores called inside functions https://github.com/ropensci/drake/issues/675#issuecomment-458222414
 
+future::plan(future::multiprocess)
 plan <- drake_plan(
     tree = ape::read.tree("data/whales_Steemanetal2009.tre"),
     fit_param_lambda7p_mu7p_crown = param_lambda7p_mu7p(desired_interval = 0.1, tree=tree, condition="crown"),
@@ -17,5 +18,5 @@ plan <- drake_plan(
         fit_param_lambda7p_mu_multiplier_lambda_stem=fit_param_lambda7p_mu_multiplier_lambda_stem
     ),
 
-    all_results_saved = save(all_results, file=file.out("results.rda"))
+    all_results_saved = save(all_results, file=file_out("results.rda"))
 )
