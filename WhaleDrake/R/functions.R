519e75352d3bb3e2333325d7d7e23ce91a711ae1
Brian O'Meara <omeara.brian@gmail.com>
2020-05-17 10:49:25 -0400

    now with param guess

--- WhaleDrake/R/functions.R
+++ WhaleDrake/R/functions.R
@@ -114,7 +114,7 @@ param_lambda7p_mu_multiplier_lambda <- function(desired_interval = 0.1, tree, co
 # desired_interval: how finely to divide the slices for calculating smoothly changing (or not) lambda and mu
 # slice_ages: when to divide regimes
 # interpolation_method: same as method in ?approx. Constant or linear
-param_lambda_discreteshift_mu_discreteshift <- function(desired_interval = 0.1, tree, condition="crown", ncores=parallel::detectCores(), slice_ages = seq(from=0, to=ceiling(castor::get_tree_span(tree)$max_distance), by=1), interpolation_method="constant") {
+param_lambda_discreteshift_mu_discreteshift <- function(desired_interval = 0.1, tree, condition="crown", ncores=parallel::detectCores(), slice_ages = seq(from=0, to=ceiling(castor::get_tree_span(tree)$max_distance), by=10), interpolation_method="constant") {
     root_age = castor::get_tree_span(tree)$max_distance
     rho = 1
     age_grid_param = seq(from=0,to=root_age+desired_interval,by=desired_interval)
@@ -165,10 +165,10 @@ param_lambda_discreteshift_mu_discreteshift <- function(desired_interval = 0.1, 
                                                fit_control       = list(rel.tol=1e-6)
                                            )
     })
-    return(list(fit_param=fit_param, lambda_function=lambda_function, mu_function=mu_function, age_grid_param=age_grid_param))
+    return(list(fit_param=fit_param, lambda_function=lambda_function, mu_function=mu_function, age_grid_param=age_grid_param, param_guess=param_guess))
 }
 
-param_lambda_discreteshift_ef_fixed <- function(desired_interval = 0.1, tree, condition="crown", ncores=parallel::detectCores(), slice_ages = seq(from=0, to=ceiling(castor::get_tree_span(tree)$max_distance), by=1), interpolation_method="constant", ef=0.0) {
+param_lambda_discreteshift_ef_fixed <- function(desired_interval = 0.1, tree, condition="crown", ncores=parallel::detectCores(), slice_ages = seq(from=0, to=ceiling(castor::get_tree_span(tree)$max_distance), by=10), interpolation_method="constant", ef=0.0) {
     root_age = castor::get_tree_span(tree)$max_distance
     rho = 1
     age_grid_param = seq(from=0,to=root_age+desired_interval,by=desired_interval)
@@ -214,5 +214,5 @@ param_lambda_discreteshift_ef_fixed <- function(desired_interval = 0.1, tree, co
                                                fit_control       = list(rel.tol=1e-6)
                                            )
     })
-    return(list(fit_param=fit_param, lambda_function=lambda_function, mu_function=mu_function, age_grid_param=age_grid_param))
+    return(list(fit_param=fit_param, lambda_function=lambda_function, mu_function=mu_function, age_grid_param=age_grid_param, param_guess=param_guess))
 }
