13cc1a1082c2ec7e12543665a6208100b73f65a3
Brian O'Meara <omeara.brian@gmail.com>
2020-07-03 22:54:18 -0400

    Extended fig 6

---
+++ ExtendedFig6Drake/make.R
@@ -0,0 +1,14 @@
+# Doing https://books.ropensci.org/drake/hpc.html#advanced-options to allow parallel within
+
+
+envir <- new.env(parent = globalenv())
+source("R/packages.R", local = envir)
+source("R/functions.R", local = envir)
+source("R/plan.R", local = envir)
+make(envir$plan, envir = envir)
+#
+# source("R/packages.R")  # loads packages
+# source("R/functions.R")
+# source("R/plan.R")      # creates the drake plan
+#
+# make(plan, parallelism = "future", jobs = parallel::detectCores())
