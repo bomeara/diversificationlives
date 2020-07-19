3319a83d48c8c5ab4daa017ed50181e418a9606d
Brian O'Meara <omeara.brian@gmail.com>
2020-07-18 21:52:22 -0400

    quick run

--- ExtendedFig6Drake/R/plan.R
+++ ExtendedFig6Drake/R/plan.R
@@ -9,7 +9,7 @@ plan <- drake_plan(
     try_many = target(
         SplitAndLikelihood(tree, nregimes=nregimes, interpolation_method=interpolation_method, type=type),
         transform = cross(
-            nregimes=c(7,10,11,12),
+            nregimes=c(7), #,10,11,12
             interpolation_method=c("linear"),
             #type=c("data", "time")
             type=c("time")
