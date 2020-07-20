f763086fb5fa4a1478f95d653b45f850c315c950
Brian O'Meara <omeara.brian@gmail.com>
2020-07-20 10:21:33 -0400

    do fewer

--- ExtendedFig6Drake/R/plan.R
+++ ExtendedFig6Drake/R/plan.R
@@ -9,7 +9,7 @@ plan <- drake_plan(
     try_many = target(
         SplitAndLikelihood(tree, nregimes=nregimes, interpolation_method=interpolation_method, type=type),
         transform = cross(
-            nregimes=c(7,10,11,12), 
+            nregimes=c(7,10,11,12),
             interpolation_method=c("linear"),
             #type=c("data", "time")
             type=c("time")
@@ -20,7 +20,7 @@ plan <- drake_plan(
         transform=combine(try_many)
     ),
     result_summary = SummarizeSplitsAndLikelihoods(everything),
-    adaptive_list = AdaptiveSampleBestModels(everything, result_summary, tree, deltaAIC_cutoff=12),
+    adaptive_list = AdaptiveSampleBestModels(everything, result_summary, tree, deltaAIC_cutoff=10),
     plot_all = PlotAll(everything, tree, file=file_out("plot.pdf")),
     plot_uncertainty = PlotAllUncertainty(everything, tree, adaptive_list, file=file_out("uncertainty.pdf")),
     save_all = save(everything, result_summary, adaptive_list,tree, session, file=file_out("everything.rda"))
