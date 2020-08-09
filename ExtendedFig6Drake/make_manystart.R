90feaea1cd378c748a1e4790c945a9fbeafb466f
Brian O'Meara <omeara.brian@gmail.com>
2020-08-09 09:29:36 -0400

    clean

--- ExtendedFig6Drake/make_manystart.R
+++ ExtendedFig6Drake/make_manystart.R
@@ -12,7 +12,7 @@
 source("R/packages.R")  # loads packages
 source("R/functions.R")
 source("R/plan.R")      # creates the drake plan
-
+drake::clean()
 options(clustermq.scheduler = "multicore")
 
 make(plan_manystart, parallelism = "clustermq", jobs = parallel::detectCores())
