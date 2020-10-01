0e70c4d1335728316d62cee0bd47a64cf628e309
Brian O'Meara <omeara.brian@gmail.com>
2020-10-01 16:55:33 -0400

    testing parallel

--- ExtendedFig6Drake/makeHPC_ManualParallelization.R
+++ ExtendedFig6Drake/makeHPC_ManualParallelization.R
@@ -4,37 +4,25 @@ Sys.setenv('R_MAX_VSIZE'=32000000000)
 
 set.seed(as.integer(sqrt(as.numeric(gsub("\\.", "", as.character(ipify::get_ip()))))))
 
-source("R/packages.R")
-source("R/functions.R")
-
-
-#future::plan(cluster, workers=cl)
+# source("R/packages.R")
+# source("R/functions.R")
 
 
 
 
 
+# load("InputForAdaptive.rda") #contains subset_list and tree
 
+# results <- parallel::parLapplyLB(cl=cl, AdaptiveSupport, fun=gethostname) # AdaptiveSupport <- function(fitted.model, tree, delta=2
 
-# now stop with drake given its issues with parallel
+# subset_list <- sample(subset_list, replace=FALSE) #randomize order
 
-#workers <- sample(c(rep(c("omearaclustera.local", "omearaclusterb.local", "omearaclusterh.local", "omearaclusterl.local", "omearaclusterk.local"), 2), rep(c("omearatc1.local", "omearatc2.local"),1)))
-#cl <- makeClusterPSOCK(workers)
+# results <- list()
 
-#parallel::clusterSetRNGStream(cl=cl)
+# for (i in seq_along(subset_list)) {
+# 	results[[i]] <- AdaptiveSupport(subset_list[[i]], tree=tree, delta=10, n_per_rep=parallel::detectCores(), n_per_good=3*parallel::detectCores(), cache_file=paste0("temp_", Sys.info()['nodename'], ".rda"))
+# 	save(results, file=paste0("adaptive_", Sys.info()['nodename'], ".rda"))
+# }
 
-#clusterExport(cl, list(ls()))
-
-
-load("InputForAdaptive") #contains subset_list and tree
-
-results <- parallel::parLapplyLB(cl=cl, AdaptiveSupport, fun=gethostname) # AdaptiveSupport <- function(fitted.model, tree, delta=2
-
-subset_list <- sample(subset_list, replace=FALSE) #randomize order
-
-results <- list()
-
-for (i in seq_along(subset_list)) {
-	results[[i]] <- AdaptiveSupport(subset_list[[i]], tree=tree, delta=10, n_per_rep=parallel::detectCores(), n_per_good=3*parallel::detectCores(), cache_file=paste0("temp_", Sys.info()['nodename'], ".rda"))
-	save(results, file=paste0("adaptive_", Sys.info()['nodename'], ".rda"))
-}
+rando <- runif(1)
+save(rando, file=paste0("rando_", Sys.info()['nodename'], ".rda"))
