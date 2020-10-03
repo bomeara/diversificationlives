b70ae4313ac78ecf2b38a5b02b7fa13e59545721
Brian O'Meara <omeara.brian@gmail.com>
2020-10-03 09:20:29 -0400

    delta 2 runs

---
+++ ExtendedFig6Drake/makeHPC_ManualParallelization_delta2.R
@@ -0,0 +1,32 @@
+# Doing https://books.ropensci.org/drake/hpc.html#advanced-options to allow parallel within
+
+Sys.setenv('R_MAX_VSIZE'=32000000000)
+
+set.seed(as.integer(round(runif(1, min=1, max=1e5)) + sqrt(as.numeric(gsub("\\.", "", as.character(ipify::get_ip()))))))
+
+setwd("/share/diversificationlives/ExtendedFig6Drake")
+
+# ansible linux -a 'nohup Rscript /share/diversificationlives/ExtendedFig6Drake/makeHPC_ManualParallelization.R &'
+
+
+source("R/packages.R")
+source("R/functions.R")
+
+
+
+
+
+load("InputForAdaptive.rda") #contains subset_list and tree
+
+subset_list <- sample(subset_list, replace=FALSE) #randomize order
+
+results <- list()
+
+for (i in seq_along(subset_list)) {
+	results[[i]] <- AdaptiveSupport(subset_list[[i]], tree=tree, delta=2, n_per_rep=5*parallel::detectCores(), n_per_good=10*parallel::detectCores(), cache_file=paste0("temp_delta2_", Sys.info()['nodename'], ".rda"))
+	save(results, file=paste0("adaptive_delta2_", Sys.info()['nodename'], ".rda"))
+}
+
+#rando <- runif(1)
+#save(rando, file=paste0("rando_", Sys.info()['nodename'], ".rda"))
+#Sys.sleep(60)
