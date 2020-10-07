299cdec03b8de7b209ece40923be1cdb6741494d
Brian O'Meara <omeara.brian@gmail.com>
2020-10-07 16:07:12 -0400

    drake parallel is fun

--- PlantDrake/R/plan.R
+++ PlantDrake/R/plan.R
@@ -1,6 +1,5 @@
 # Note that drake will not work with multiple cores called inside functions https://github.com/ropensci/drake/issues/675#issuecomment-458222414
 
-future::plan(future::multiprocess)
 plan <- drake_plan(
     tree = ape::read.tree("data/GBMB.ultra.tre"),
     #many_regimes = TryManyRegimes(tree, maxregimes=13),
--- PlantDrake/make.R
+++ PlantDrake/make.R
@@ -1,14 +1,16 @@
 # Doing https://books.ropensci.org/drake/hpc.html#advanced-options to allow parallel within
 
 
-envir <- new.env(parent = globalenv())
-source("R/packages.R", local = envir)
-source("R/functions.R", local = envir)
-source("R/plan.R", local = envir)
-make(envir$plan, envir = envir, jobs = 12)
-#
-# source("R/packages.R")  # loads packages
-# source("R/functions.R")
-# source("R/plan.R")      # creates the drake plan
-#
-# make(plan, parallelism = "future", jobs = parallel::detectCores())
+future::plan(future::multiprocess)
+
+# envir <- new.env(parent = globalenv())
+# source("R/packages.R", local = envir)
+# source("R/functions.R", local = envir)
+# source("R/plan.R", local = envir)
+# make(envir$plan, envir = envir, jobs = 12)
+
+source("R/packages.R")  # loads packages
+source("R/functions.R")
+source("R/plan.R")      # creates the drake plan
+
+make(plan, parallelism = "future", jobs = parallel::detectCores())
