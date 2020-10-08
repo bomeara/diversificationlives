c8cc594db7787f07eaf99a884eb9b27ee6f9d5da
Brian O'Meara <omeara.brian@gmail.com>
2020-10-08 12:57:13 -0400

    updating path

--- PlantDrake/summarizeRuns.R
+++ PlantDrake/summarizeRuns.R
@@ -1,4 +1,4 @@
 library(drake)
 setwd("/share/diversificationlives/PlantDrake")
 loadd(path="~/Documents/localcache")
-save(list=ls(), paste0(system("hostname", intern=TRUE), "_inprocess_everything.rda"))
+save(list=ls(), file=paste0(system("hostname", intern=TRUE), "_inprocess_everything.rda"))
