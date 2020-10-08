920f0ad165b5eb50eaed8081aa29479c25963aab
Brian O'Meara <omeara.brian@gmail.com>
2020-10-08 14:21:07 -0400

    send summary

--- PlantDrake/summarizeRuns.R
+++ PlantDrake/summarizeRuns.R
@@ -1,4 +1,9 @@
 library(drake)
 setwd("/share/diversificationlives/PlantDrake")
 loadd(path="~/Documents/localcache")
+print(ls())
 save(list=ls(), file=paste0("ef_", system("hostname", intern=TRUE), "_inprocess_everything.rda"))
+
+# ansible linux -a 'nohup Rscript /share/diversificationlives/PlantDrake/summarizeRuns.R &'
+
+# then on one computer, summarizeRunsStep2.R
