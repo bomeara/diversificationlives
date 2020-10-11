72a8b9538b6c38530ace41753ff17b167956b650
Brian O'Meara <omeara.brian@gmail.com>
2020-10-11 00:42:41 -0400

    summary

--- PlantDrake/summarizeRuns.R
+++ PlantDrake/summarizeRuns.R
@@ -1,6 +1,7 @@
 library(drake)
 setwd("/share/diversificationlives/PlantDrake")
 loadd(path="~/Documents/localcache")
+print(system("hostname", intern=TRUE))
 print(ls())
 save(list=ls(), file=paste0("ef_", system("hostname", intern=TRUE), "_inprocess_everything.rda"))
 
