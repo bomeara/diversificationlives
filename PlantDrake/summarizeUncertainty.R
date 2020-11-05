3f2b1c34c3445adb0eebf99eaecfb40d65929d95
Brian O'Meara <omeara.brian@gmail.com>
2020-11-05 13:32:52 -0500

    summarize

--- PlantDrake/summarizeUncertainty.R
+++ PlantDrake/summarizeUncertainty.R
@@ -1,19 +1,23 @@
-library(drake)
 setwd("/share/diversificationlives/PlantDrake")
+rm(list=ls())
 source("R/packages.R")
 source("R/functions.R")
 files <- list.files(pattern="ef_adaptive_new_.*.rda")
 for(i in seq_along(files)) {
-	load(files[i])
-	everything_adaptive_compiled$source <- files[i]
-	if(i==1) {
-		global_everything_adaptive_compiled <- everything_adaptive_compiled
-	} else {
-		for(run_index in seq_along(global_everything_adaptive_compiled)) {
-			global_everything_adaptive_compiled[[run_index]] <- rbind(global_everything_adaptive_compiled[[run_index]], everything_adaptive_compiled[[run_index]])
+	try(load(files[i]))
+	if(exists("everything_adaptive_compiled")) {
+		for(element_index in seq_along(everything_adaptive_compiled)) {
+			element_adaptive_compiled[[element_index]]$source = files[i]
 		}
+		if(i==1) {
+			global_everything_adaptive_compiled <- everything_adaptive_compiled
+		} else {
+			for(run_index in seq_along(global_everything_adaptive_compiled)) {
+				global_everything_adaptive_compiled[[run_index]] <- rbind(global_everything_adaptive_compiled[[run_index]], everything_adaptive_compiled[[run_index]])
+			}
+		}
+		rm(everything_adaptive_compiled)
 	}
-	rm(everything_adaptive_compiled)
 }
 save(list=ls(), file="global_adaptive_new.rda")
 PlotAllUncertainty(everything_adaptive, tree, global_everything_adaptive_compiled,  file=paste0("global_adaptive_new_UNCERTAINTY_2_plot.pdf"), desired_delta=2)
