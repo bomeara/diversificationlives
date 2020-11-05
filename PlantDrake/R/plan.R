40f56d84de0b51663ac70ca970d2d29e3b1b2375
Brian O'Meara <omeara.brian@gmail.com>
2020-11-05 10:50:20 -0500

    extra parens

--- PlantDrake/R/plan.R
+++ PlantDrake/R/plan.R
@@ -66,8 +66,8 @@ plan_adaptive <- drake_plan(
         transform=combine(adaptive_list_new)
     ),
     save_adaptive = save(tree, session, result_summary_adaptive_new, everything_adaptive, everything_adaptive_new, adaptive_list_new, file=paste0("ef_adaptive_new_", system("hostname", intern=TRUE), ".rda")),
-	plot_uncertainty2 = PlotAllUncertainty(everything_adaptive_new, tree, adaptive_list_new,  file=paste0("ef_adaptive_new", system("hostname", intern=TRUE), "_2_plot.pdf")), desired_delta=2),
-	plot_uncertainty5 = PlotAllUncertainty(everything_adaptive_new, tree, adaptive_list_new,  file=paste0("ef_adaptive_new", system("hostname", intern=TRUE), "_5_plot.pdf")), desired_delta=5)
+	plot_uncertainty2 = PlotAllUncertainty(everything_adaptive_new, tree, adaptive_list_new,  file=paste0("ef_adaptive_new", system("hostname", intern=TRUE), "_2_plot.pdf"), desired_delta=2),
+	plot_uncertainty5 = PlotAllUncertainty(everything_adaptive_new, tree, adaptive_list_new,  file=paste0("ef_adaptive_new", system("hostname", intern=TRUE), "_5_plot.pdf"), desired_delta=5)
 )
 
 
