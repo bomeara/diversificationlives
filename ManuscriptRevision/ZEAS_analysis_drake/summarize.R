0c8c0864a1689deec17c3607a75d78fda8da17a3
Brian O'Meara <omeara.brian@gmail.com>
2022-06-16 15:15:57 -0400

    updating with summary

--- ManuscriptRevision/ZEAS_analysis_drake/summarize.R
+++ ManuscriptRevision/ZEAS_analysis_drake/summarize.R
@@ -1,39 +1,33 @@
 library(hisse)
+library(Metrics)
 
 
-all_results_model_average <- data.frame()
 all_results_best <- data.frame()
-all_results_overview <- data.frame()
+aggregate_results_best <- data.frame()
 
 inputs <- list.files(pattern="omeara.*results.rda", full.names=TRUE)
 
 for (i in sequence(length(inputs))) {
   load(inputs[i])
   for(j in sequence(length(results))) {
-	local <- hisse::SummarizeMiSSEGreedy(results[[j]]$greedy)
-	local_model_average <- as.data.frame(local$rates[,,"model_average"])
-	local_model_average$input_file <- inputs[i]
-	local_model_average$rep_number <- j
-	local_model_average$Ntip <- ape::Ntip(results[[j]]$phy)
-	all_results_model_average <- rbind(all_results_model_average, local_model_average)
+	local_best <- as.data.frame(results[[j]]$summaries$rates[,,"best"])
+	local_best$speciation_true <- results[[j]]$lambda[length(results[[j]]$lambda)]
+	local_best$extinction_true <- 0
+	local_best$final_time <- results[[j]]$final_time
+	local_best$crown_time <- results[[j]]$root_time
+	local_best$turnover_true <- local_best$speciation_true + local_best$extinction_true
+	local_best$extinction.fraction_true <- local_best$extinction_true/local_best$speciation_true 
+	local_best$net.div_true <- local_best$speciation_true - local_best$extinction_true
+	local_best$input_file <- inputs[i]
+	local_best$rep_number <- j
+	local_best <- local_best[sequence(ceiling(nrow(local_best)/2)),] # to get only the tips
+	local_best$taxon_number <- sequence(nrow(local_best))
+	all_results_best <- rbind(all_results_best, local_best)
+	aggregate_results_best <- rbind(aggregate_results_best, data.frame(net.div_range=diff(range(local_best$net.div)), net.div_rmse=Metrics::rmse(local_best$net.div, local_best$net.div_true), net.div_median = median(local_best$net.div), net.div_true=local_best$net.div_true[1],input_file = inputs[i], rep_number = j))
 	
-	local_model_best <- as.data.frame(local$rates[,,"best"])
-	local_model_best$input_file <- inputs[i]
-	local_model_best$rep_number <- j
-	local_model_best$Ntip <- ape::Ntip(results[[j]]$phy)
-
-	all_results_best <- rbind(all_results_best, local_model_best)
-	
-	local_overview <- local$overview
-	local_overview$input_file <- inputs[i]
-	local_overview$rep_number <- j
-	local_overview$Ntip <- ape::Ntip(results[[j]]$phy)
-
-	all_results_overview <- rbind(all_results_overview, local_overview)
-	save(all_results_best, file="all_results_best.rda")
-  	save(all_results_model_average, file="all_results_model_average.rda")
-  	save(all_results_overview, file="all_results_overview.rda")
-  	print(paste0("Finished ", inputs[i], " rep ", j))
   }
-
+  save(all_results_best, file="all_results_best.rda")
+  save(aggregate_results_best, file="aggregate_results_best.rda")
+  print(paste0("Finished ", inputs[i], " rep ", j))
 }
+
